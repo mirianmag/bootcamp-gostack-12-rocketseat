33477dd08746dcb7d6fb54b46a95e93ba026f3e5
Mirian Magri <magri.mirian@gmail.com>
2020-06-17 10:06:04 -0400

    creating a project in the route POST

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,25 +1,29 @@
 const express = require('express');
+const { uuid } = require('uuidv4');
 
 const app = express();
 
 app.use(express.json());
 
+const projects = [];
+
 app.get('/projects', (request, response) => {
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-  ]);
+
+  return response.json(projects);
 });
 
 app.post('/projects', (request, response) => {
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+  const { title, owner } = request.body;
+
+  const project = { id: uuid(), title, owner };
+  projects.push(project);
+
+  return response.json(project);
 });
 
 app.put('/projects/:id', (request, response) => {
+ 
+
   return response.json([
     'Projeto 4',
     'Projeto 2',
