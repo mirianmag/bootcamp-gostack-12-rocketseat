09ab9423517c9f4d57db6f6b8d6f1a694237f93c
Mirian Magri <magri.mirian@gmail.com>
2020-06-17 10:40:34 -0400

    implementing the DELETE route for delete project by ID

--- backend/src/index.js
+++ backend/src/index.js
@@ -43,10 +43,17 @@ app.put('/projects/:id', (request, response) => {
 });
 
 app.delete('/projects/:id', (request, response) => {
-  return response.json([
-    'Projeto 4',
-    'Projeto 2',
-  ]);
+  const { id } = request.params;  
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found. '});
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.listen(3333, () => {
