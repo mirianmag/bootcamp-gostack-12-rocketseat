8e21a423d74c0b48c482677a57e8f635cbb1b0ed
Mirian Magri <magri.mirian@gmail.com>
2020-06-19 16:26:23 -0400

    Configuring Babel, Webpac, Componentization, Properties, State and immutability

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+import Header from './components/Header';
+
+function App() {
+  const [projects, setProjects ] = useState(['Desenv', 'Front']);
+
+
+  function handleAddProject() {
+
+    setProjects([ ...projects, `Novo projeto ${Date.now()}`]);
+
+    console.log(projects);
+  }
+  return (
+    <>
+      <Header title="Projects"/>   
+      <ul>
+        {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
+    </>
+  );
+}
+
+export default App;
