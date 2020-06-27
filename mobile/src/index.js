df57b77ead85ae562d08b9588b1f25d051e24201
Mirian Magri <magri.mirian@gmail.com>
2020-06-27 19:06:18 -0400

    creating first page :+1:

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
 
 
 export default function App() {
-  return <View />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159C1"/>
+      <View style={styles.container}>
+        <Text style= {styles.title}>Hello</Text>
+      </View>
+    </>
+    );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159C1',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  title: {
+    color: "#FFF",
+    fontSize: 32,
+    fontWeight: 'bold',
+  }
+});
