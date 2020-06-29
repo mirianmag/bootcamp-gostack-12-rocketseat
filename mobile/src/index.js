b6876ebe8f975d584c7c52cf597180a083fa7de5
Mirian Magri <magri.mirian@gmail.com>
2020-06-29 15:54:28 -0400

    listing projects API react native

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native';
 
+import api from './services/api';
 
 export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+        setProjects(response.data);
+    });
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159C1"/>
-      <View style={styles.container}>
-        <Text style= {styles.title}>Hello</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <FlatList 
+          data={projects}
+          keyExtractor={project => project.id}
+          renderItem={({ item }) => (
+            <Text style={styles.project}>{item.title}</Text>
+          )}
+        />
+      </SafeAreaView>
+     
     </>
     );
 }
@@ -17,13 +33,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#7159C1',
-    justifyContent: 'center',
-    alignItems: 'center',
   },
 
-  title: {
+  project: {
     color: "#FFF",
-    fontSize: 32,
-    fontWeight: 'bold',
+    fontSize: 20,
   }
 });
